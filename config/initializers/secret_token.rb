545b3ef2ba8ac701156b3fe32b12c4bc638983a5
ITX351 <itx351@126.com>
2015-10-28 13:12:09 -0300

    Project Created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tardis::Application.config.secret_token = 'aefffcfc300cc7e1a31cdd1656328482cfe43d945fc201d33cb58bd2d1339a57e79b989539fa72c43d1f65ca4fc67e522b4c4e10ae091a68652852608971d499'
