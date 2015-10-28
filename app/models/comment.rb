c98f4d2c1aefcb241c8f699832dd219cccdb6ca6
ITX351 <itx351@126.com>
2015-10-28 14:15:53 -0300

    Add relation between users and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   attr_accessible :article_id, :content, :status, :user_id
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :classes, :email, :name, :password
+  has_many :comments
 end
