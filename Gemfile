cc18aa7409c0f711da07caa04c82349f304e9e79
ITX351 <itx351@126.com>
2015-10-29 15:04:57 +0800

    Encrypt setuped

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.2.16'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
+gem 'bcrypt-ruby', '3.0.1'
 
 group :development, :test do
 	gem 'rspec-rails', '2.13.1'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :classes, :email, :name, :password
   has_many :comments
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true
+  validates :classes, presence: true
+
+  #email validates abc@def.com
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  
 end
