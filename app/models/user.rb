7ea489c360c891cd6d01be465e3b23ef311bba9c
ITX351 <itx351@126.com>
2015-10-29 15:21:04 +0800

    Password encrypted and records updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :classes, :email, :name, :password
   has_many :comments
+  has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :password, presence: true
@@ -9,5 +10,5 @@ class User < ActiveRecord::Base
   #email validates abc@def.com
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151028170751) do
+ActiveRecord::Schema.define(:version => 20151029070712) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(:version => 20151028170751) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.string   "password"
+    t.string   "password_digest"
     t.integer  "classes"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
