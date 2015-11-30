3c20a90a9b5a18780a17ac0966b2f4280b874f07
Walter Shi <shaoshuai_1993@163.com>
2015-12-01 01:01:59 +0800

    modified production complie false->true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Tardis::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true   # yuanben shi  false!!!!!!! by sss
 
   # Generate digests for assets URLs
   config.assets.digest = true
