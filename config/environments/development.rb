c779a31e0d4e533694402d275b0df7f093834ab6
ITX351 <itx351@126.com>
2015-12-19 17:20:35 +0800

    config changed?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,8 @@ Tardis::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
+  
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,15 @@ Tardis::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address => "smtp.163.com",
+     :port => 25,
+     :domain => "163.com",
+     :authentication => :login,
+     :user_name => "se_tardis",
+     :password => "goodhigh"
+   }
+
 end
