f37ffcc4686ad1804ce461224b72a8b2386e57c2
Walter Shi <shaoshuai_1993@163.com>
2015-12-20 13:28:20 +0800

    update the model user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+	# Include default devise modules. Others available are:
+	# :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	     :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :nickname
-  # attr_accessible :title, :body
-  
-  has_many :comments
-  ajaxful_rater
+	# Setup accessible (or protected) attributes for your model
+	attr_accessible :email, :password, :password_confirmation, :remember_me, :nickname
+	# attr_accessible :title, :body
+
+	has_many :comments
+	ajaxful_rater
 end
