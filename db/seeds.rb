165d7980791d37642dc7c28b842963a386651e07
ITX351 <itx351@126.com>
2015-12-22 18:50:27 +0800

    seed bug fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,11 @@
 
 Placeclassify.create!(:name1 => "餐厅", :name2 => "食堂", :instruction => "", :imagename => "dininghall.png")
 Placeclassify.create!(:name1 => "理发店", :name2 => "美发店", :instruction => "", :imagename => "barbershop.png")
-Placeclassify.create!(:name1 => "教学楼", :instruction => "building.png")
-Placeclassify.create!(:name1 => "公寓", :name2 => "寝室", :name3 => "宿舍", :instruction => "dormitory.png")
+Placeclassify.create!(:name1 => "教学楼", :instruction => "", :imagename => "building.png")
+Placeclassify.create!(:name1 => "公寓", :name2 => "寝室", :name3 => "宿舍", :instruction => "", :imagename => "dormitory.png")
 Placeclassify.create!(:name1 => "快递", :instruction => "", :imagename => "express.png")
-Placeclassify.create!(:name1 => "打印店", :name2 => "打印社", :instruction => "printstore.png")
-Placeclassify.create!(:name1 => "超市", :name2 => "便利店", :instruction => "supermarket.png")
-Placeclassify.create!(:name1 => "图书馆", :instruction => "library.png")
-Placeclassify.create!(:name1 => "电影院", :instruction => "cinema.png")
+Placeclassify.create!(:name1 => "打印店", :name2 => "打印社", :instruction => "", :imagename => "printstore.png")
+Placeclassify.create!(:name1 => "超市", :name2 => "便利店", :instruction => "", :imagename => "supermarket.png")
 
 =begin
 学苑楼
